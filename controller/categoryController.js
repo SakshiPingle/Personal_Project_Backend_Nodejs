29dcd8004331f5e3a305625aefd36b657fa9afb7
SakshiPingle <sakshipingle603@gmail.com>
2025-08-25 03:45:52 +0530

    category done

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,16 +1,92 @@
-exports.createCategory = ((res,req,next)=>{
+const Category = require('../models/categoryModel')
 
+exports.createCategory = ((req,res,next)=>{
+
+ let category = new Category({
+    category_name : req.body.category_name,
+    category_id : req.body.uniqueID
+ })
+ category.save()
+ .then((data)=>{
+    res.status(200).json({
+        message:"Category Added Succesfully",
+        data:data
+    })
+ }).catch(()=>{
+    res.status(200).json({
+        message:"Failed Adding Category",
+        data:[]
+    })
+ })
 })
 
-exports.updateCategory = ((res,req,next)=>{
-
+exports.updateCategory = ((req,res,next)=>{
+    console.log(req.body)
+    let category_id = req.body.category_id;
+    let category_name =  req.body.category_name
+   Category.update(
+    {category_name :category_name},
+    {where:{category_id :category_id}}
+   )
+    .then((data)=>{
+    res.status(200).json({
+        message:"Category Updated Succesfully",
+        data:data
+    })
+ }).catch(()=>{
+    res.status(200).json({
+        message:"Failed Updating Category",
+        data:[]
+    })
+ })
 })
 
 
-exports.deleteCategory = ((res,req,next)=>{
+exports.deleteCategory = (req, res, next) => {
+  let category_id = req.params.category_id;
+  Category.destroy({ where: { category_id: category_id } })
+    .then((data) => {
+      res.status(200).json({
+        message: "Category Deleted Successfully",
+        data: data
+      });
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Failed Deleting Category",
+        data: []
+      });
+    });
+};
 
-})
  
-exports.getCategoryList = ((res,req,next)=>{
+exports.getCategoryList = ((req,res,next)=>{
+  Category.findAll()
+  .then((data)=>{
+    res.status(200).json({
+        message:"Category List Fetched Succesfully",
+        data:data
+    })
+  }).catch(()=>{
+      res.status(200).json({
+        message:"Failed Fetching Category List",
+        data:[]
+    })
+  })
+})
 
+exports.getCategoryById = ((req,res,next)=>{
+    let category_id = req.params.category_id;
+    Category.findOne({category_id : category_id})
+     .then((data)=>{
+    res.status(200).json({
+        message:"Category Fetched Succesfully",
+        data:data
+    })
+ }).catch(()=>{
+    res.status(200).json({
+        message:"Failed Fetching Category",
+        data:[]
+    })
+ })
 })
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controller/categoryController')
 
-router.post('create_category',CategoryController.createCategory);
-router.put('update_category',CategoryController.updateCategory)
-router.delete('delete_category',CategoryController.deleteCategory)
-router.get('get_category_list',CategoryController.getCategoryList);
+router.post('/create_category',CategoryController.createCategory);
+router.put('/update_category',CategoryController.updateCategory)
+router.delete('/delete_category/:category_id',CategoryController.deleteCategory)
+router.get('/get_category_list',CategoryController.getCategoryList);
+router.get('/get_edit_category/:category_id',CategoryController.getCategoryById)
 
 module.exports = router;
